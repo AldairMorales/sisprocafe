6597324692b2378b4da572ef7701e6cd7fad97d6
Carlos Chininin <carloschininin@gmail.com>
2022-02-14 23:31:44 -0500

    Trabjando con tipos

--- src/Form/AnalisisFisicoType.php
+++ src/Form/AnalisisFisicoType.php
@@ -7,17 +7,17 @@
 
 namespace Pidia\Apps\Demo\Form;
 
+use Doctrine\ORM\EntityRepository;
 use Pidia\Apps\Demo\Entity\AnalisisFisico;
 use Pidia\Apps\Demo\Entity\UnidadMedida;
+use Pidia\Apps\Demo\Form\Type\ValorMedidaType;
 use Pidia\Apps\Demo\Security\Security;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Doctrine\ORM\EntityRepository;
-use App\Form\Type\ShippingType;
 
 class AnalisisFisicoType extends AbstractType
 {
@@ -43,8 +43,8 @@ class AnalisisFisicoType extends AbstractType
                 'choices' => [
                     '100' => 1,
                     '300' => 2,
-                    '400' => 3
-                ]
+                    '400' => 3,
+                ],
             ])
 
             ->add('unidadMedida', EntityType::class, [
@@ -78,7 +78,13 @@ class AnalisisFisicoType extends AbstractType
             ])
 
             ->add('humedad')
-            ->add('descripcion');
+            ->add('descripcion')
+            ->add('exportableDemo', ValorMedidaType::class, [
+                'mapped' => false,
+            ])
+            ->add('segundaDemo', ValorMedidaType::class, [
+                'mapped' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
